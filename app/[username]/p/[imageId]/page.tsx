79802f1a3c3c86b13a7cff2cf8a1bc92f77a3d24
iamfiro <appturbo102@gmail.com>
2024-02-28 11:31:32 +0900

    💄 Edit favicon

--- app/[username]/p/[imageId]/page.tsx
+++ app/[username]/p/[imageId]/page.tsx
@@ -3,19 +3,53 @@
 import CosmosPageTitle from "@/components/cosmos/header";
 import styles from "@/styles/cosmos/detail/page.module.scss";
 import Image from "next/image";
-import dummy from '@/public/image/dummy/photo_1.jpg';
+import dummy from '@/public/image/dummy/photo_3.jpg';
 import CosmosPageProfile from "@/components/cosmos/profile";
 import { useEffect } from "react";
 import gsap from "gsap";
 import Link from "next/link";
 
-function CameraDetail() {
+import { FaCamera } from "react-icons/fa6";
+import { PiFilmReelBold } from "react-icons/pi";
+import { FaRunning } from "react-icons/fa";
+import { RiCameraLensFill } from "react-icons/ri";
+import { MdOutlineLensBlur } from "react-icons/md";
+import { MdPhotoSizeSelectLarge } from "react-icons/md";
+
+interface CameraDetailProps {
+    username: string;
+    camera: string;
+    film: string;
+}
+
+function CameraDetail({ username, camera, film }: CameraDetailProps) {
     return (
         <div className={styles.cameraDetailWrap}>
-            <span className={styles.imageDetail}>35mm <strong>53mm</strong></span>
-            <span className={styles.imageDetail}>ƒ/1.4</span>
-            <span className={styles.imageDetail}>1/250s</span>
-            <span className={styles.imageDetail}>ISO 120</span>
+            <div>
+                <FaCamera />
+                <Link href={`/${username}/camera/${camera}`} className={`${styles.imageDetail} ${styles.cameraName}`}>{camera}</Link>
+            </div>
+            <div>
+                <PiFilmReelBold />
+                <Link href={`/${username}/film/${film}`} className={`${styles.imageDetail} ${styles.cameraFilm}`}>{film}</Link>
+            </div>
+            <div style={{ height: '10px'}}/>
+            <div>
+                <MdPhotoSizeSelectLarge />
+                <span className={styles.imageDetail}>35mm <strong>53mm</strong></span>
+            </div>
+            <div>
+                <RiCameraLensFill />
+                <span className={styles.imageDetail}>ƒ/1.4</span>
+            </div>
+            <div>
+                <FaRunning />
+                <span className={styles.imageDetail}>1/250s</span>
+            </div>
+            <div>
+                <MdOutlineLensBlur />
+                <span className={styles.imageDetail}>ISO 120</span>
+            </div>
         </div>
     )
 }
@@ -41,22 +75,18 @@ export default function CosmosDetailImage({ params }: { params: { username: stri
                         alt="image name"
                         className={styles.image}
                     />
+                    <span className={styles.adTitle}>이런 제품은 어떠신가요?</span>
                 </div>
                 <div className={styles.imageDetailContainer}>
                     <div className={styles.imageTypeContainer}>PHOTO</div>
                     <h1 className={styles.imageName}>이미지 이름</h1>
-                    <div style={{ height: '15px'}}/>
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <Link href={`/${params.username}/camera/${camera}`} className={`${styles.imageDetail} ${styles.cameraName}`}>{camera}</Link>
-                        <Link href={`/${params.username}/film/${film}`} className={`${styles.imageDetail} ${styles.cameraFilm}`}>{film}</Link>
-                    </div>
                     <div style={{ height: '10px'}}/>
                     <span className={styles.imageDetail}>2982x1829</span>
                     <div style={{ height: '10px'}}/>
-                    <CameraDetail />
-                    <div style={{ height: '15px'}}/>
+                    <CameraDetail username={params.username} camera={camera} film={film} />
+                    <div style={{ height: '10px'}}/>
                     <span className={styles.imageDetail}>2024/03/23 18:32PM</span>
-                    <div style={{ height: '30px'}}/>
+                    <div style={{ height: '10px'}}/>
                     <span className={styles.imageDetail}>1,920 Views</span>
                 </div>
             </section>
